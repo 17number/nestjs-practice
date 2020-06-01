48e8444e3b0ef66588172dd092a95737f25f902b
r17n <4716989+17number@users.noreply.github.com>
2020-06-01 22:38:45 +0900

    feat: Custom response header example

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,17 +1,16 @@
-import { Controller, Get, Post, Req, HttpCode } from '@nestjs/common';
+import { Controller, Get, Post, Req, Header } from '@nestjs/common';
 import { FastifyRequest } from 'fastify';
 
 @Controller('cats')
 export class CatsController {
   @Get()
-  @HttpCode(204)
+  @Header('Foo', 'Bar')
   findAll(@Req() request: FastifyRequest): string {
     console.log({request});
     return 'All cats';
   }
 
   @Post()
-  @HttpCode(202)
   create(): string {
     return 'New cat';
   }
