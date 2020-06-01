9aacf4ebc0aee3bbeba07a3bc9c5d0321eebfaf6
r17n <4716989+17number@users.noreply.github.com>
2020-06-01 22:33:00 +0900

    feat: change status code example

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Get, Post, Req, HttpCode } from '@nestjs/common';
 import { FastifyRequest } from 'fastify';
 
 @Controller('cats')
 export class CatsController {
   @Get()
+  @HttpCode(204)
   findAll(@Req() request: FastifyRequest): string {
     console.log({request});
     return 'All cats';
   }
 
   @Post()
+  @HttpCode(202)
   create(): string {
     return 'New cat';
   }
