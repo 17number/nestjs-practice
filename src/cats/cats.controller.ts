c8c5ce5fba581be0346e1466e9d135df2cbef53c
r17n <4716989+17number@users.noreply.github.com>
2020-06-02 23:35:38 +0900

    feat: async example

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,24 +1,19 @@
-import { Controller, Get, Post, Req, Header, Redirect } from '@nestjs/common';
-import { FastifyRequest } from 'fastify';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   @Get()
-  @Header('Foo', 'Bar')
-  findAll(@Req() request: FastifyRequest): string {
-    console.log({request});
+  findAll(): string {
     return 'All cats';
+  }
+
+  @Get('async')
+  async findAllAsync(): Promise<string> {
+    return 'All cats(async)';
   }
 
   @Post()
   create(): string {
     return 'New cat';
-  }
-
-  @Get('redirect')
-  @Redirect('/cats')
-  redirect() {
-    console.log('redirect override');
-    return { url: '/', statusCode: 301 };
   }
 }
