a8e905db75ffe302cc123f092b9b20e59e2c9c2f
r17n <4716989+17number@users.noreply.github.com>
2020-06-03 00:04:17 +0900

    feat: DTO example

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,19 +1,25 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Query } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+import { ListAllEntities } from './dto/list-all-entities.dto';
 
 @Controller('cats')
 export class CatsController {
   @Get()
-  findAll(): string {
+  findAll(@Query() query: ListAllEntities): string {
+    // some logic
     return 'All cats';
   }
 
-  @Get('async')
-  async findAllAsync(): Promise<string> {
-    return 'All cats(async)';
+  @Post()
+  create(@Body() createCatDto: CreateCatDto): string {
+    // some logic
+    return 'New cat';
   }
 
-  @Post()
-  create(): string {
-    return 'New cat';
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    // some logic
+    return `Update cat(id ${id})`;
   }
 }
